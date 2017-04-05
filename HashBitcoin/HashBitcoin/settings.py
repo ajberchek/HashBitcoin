63ceb175778075ef667e5c74c239ed9b8aa02316
ajberchek <ajberchek@gmail.com>
2017-04-05 10:21:29 -0700

    Middleware is now being used and we can create our own

--- HashBitcoin/HashBitcoin/settings.py
+++ HashBitcoin/HashBitcoin/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'HomeScreen.middleware.client_puzzle_bitcoin.ClientPuzzleBitcoinMiddleware',
 ]
 
 ROOT_URLCONF = 'HashBitcoin.urls'
---
+++ HashBitcoin/HomeScreen/middleware/client_puzzle_bitcoin.py
@@ -0,0 +1,11 @@
+class ClientPuzzleBitcoinMiddleware(object):
+    def __init__(self,get_response):
+        self.get_response = get_response
+
+    def __call__(self,request):
+
+        response = self.get_response(request)
+
+        print("YOYOYOYO we made it to middleware!!")
+
+        return response
